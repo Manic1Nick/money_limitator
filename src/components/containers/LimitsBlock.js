f396c976b9f678a81e18d6f1e53f585bc731df5e
Nick <nick.no.cave@gmail.com>
2018-02-20 20:40:03 +0200

    add dialog Limit info

--- src/components/containers/LimitsBlock.js
+++ src/components/containers/LimitsBlock.js
@@ -2,9 +2,27 @@ import LimitsBlock from '../ui/LimitsBlock'
 import { bindActionCreators as action } from 'redux'
 import { connect } from 'react-redux'
 
+import DateUtil from '../../util/DateUtil'
+
 const mapStateToProps = (state, ownProps) => {
+
+	const lastDateWithExpense = DateUtil.getLastDate(state.expenses),
+		daysTotal = DateUtil.getDaysInPeriod(state.period),
+        daysDone = DateUtil.getDaysInPeriod({ 
+            begin: state.period.begin, end: lastDateWithExpense,  
+        }),
+		daysRest = DateUtil.getDaysInPeriod({ 
+            begin: lastDateWithExpense, end: state.period.end,  
+        })
+
     return {
-    	limits: state.limits
+    	limits: state.limits,
+    	summs: state.summs,
+    	period: state.period,
+    	daysTotal,
+    	daysDone,
+    	daysRest: daysRest - 1,
+    	lastDateWithExpense
     }
 }
 
--- src/components/ui/LimitsBlock.js
+++ src/components/ui/LimitsBlock.js
@@ -1,41 +1,171 @@
-import { PropTypes } from 'prop-types'
+import { Component } from 'react'
 
 import RaisedButton from 'material-ui/RaisedButton'
+import Dialog from 'material-ui/Dialog'
+import FlatButton from 'material-ui/FlatButton'
 
-const LimitsBlock = ({ limits={} }) => {
-	
-	let labelBase = `Base ${limits.base} in day`,
-		labelCorrected = `Corrected ${limits.corrected} in day`,
-		labelFact = `Fact ${limits.fact} in day`
+export default class LimitsBlock extends Component {
 
-	return (
-		<div className='LimitsBlock'>
-			<div className='limits__title'>
-				<span>LIMITS:</span>
+	constructor(props) {
+		super()
+		this.state = {
+			limits: props.limits,
+			activeLimit: ''
+		}
+	}
+
+	handleOpenDialog(limitName) {
+		this.setState({
+			activeLimit: limitName
+		})
+	}
+
+	handleCloseDialog() {
+		this.setState({
+			activeLimit: ''
+		})
+	}
+
+	renderDialog() {
+
+		const { activeLimit } = this.state,
+			limitInfoTitle = `Limit ${ activeLimit } info`
+
+		const actions = [
+    	  	<FlatButton
+    	  	  	label="Ok"
+    	  	  	primary={true}
+    	  	  	onClick={ this.handleCloseDialog.bind(this) }
+    	  	/>
+    	]
+
+		return(
+			<Dialog
+        	  	title={ limitInfoTitle }
+        	  	actions={ actions }
+        	  	modal={ false }
+        	  	open={ activeLimit !== '' }
+        	  	onRequestClose={ this.handleCloseDialog.bind(this) }
+        	>
+        	  	{ this._generateDialogText(activeLimit) }
+        	</Dialog>
+		)
+	}
+
+	render() {		
+		return (
+			<div className='LimitsBlock'>
+				<div className='limits__title'>
+					<span>LIMITS:</span>
+				</div>
+				<RaisedButton 
+					className='limits__button'
+					label={ this._generateLimitLable('base') } 
+					primary={true} 
+					onClick={ () => this.handleOpenDialog('base') }
+				/>
+				<RaisedButton 
+					className='limits__button'
+					label={ this._generateLimitLable('corrected') } 
+					backgroundColor="#9932CC" 
+					labelColor="#FFFFFF" 
+					onClick={ () => this.handleOpenDialog('corrected') }
+				/>
+				<RaisedButton 
+					className='limits__button'
+					label={ this._generateLimitLable('fact') } 
+					backgroundColor="#FF6347" 
+					labelColor="#FFFFFF" 
+					onClick={ () => this.handleOpenDialog('fact') }
+				/>
+				{ this.renderDialog() }
 			</div>
-			<RaisedButton 
-				className='limits__button'
-				label={labelBase} 
-				primary={true} 
-			/>
-			<RaisedButton 
-				className='limits__button'
-				label={labelCorrected} 
-				backgroundColor="#9932CC" 
-				labelColor="#FFFFFF" 
-			/>
-			<RaisedButton 
-				className='limits__button'
-				label={labelFact} 
-				backgroundColor="#FF6347" 
-				labelColor="#FFFFFF" 
-			/>
-		</div>
-	)
-}
+		)
+	}
 
-LimitsBlock.propTypes = {
-	limits: PropTypes.object
-}
+	_generateLimitLable(limitName) {
+		const { base, corrected, fact } = this.state.limits
 
-export default LimitsBlock
+		switch(limitName) {
+			case 'base':
+				return `Base ${base} in day`
+
+			case 'corrected':
+				return `Corrected ${corrected} in day`
+
+			case 'fact':
+				return `Fact ${fact} in day`
+
+			default:
+				return ''
+		}
+	}
+
+	_generateDialogText(limitName) {
+		const { base, corrected, fact } = this.state.limits,
+			{ period, summs, daysTotal, daysDone, daysRest, lastDateWithExpense } = this.props,
+			summNetIncome = summs.incomes - summs.notIncluded,
+			currentBalance = summNetIncome - summs.expenses,
+			result = currentBalance - (daysRest * fact)
+
+		switch(limitName) {
+			case 'base':
+				return (
+					<div>
+						<p>Your income is {summNetIncome} for the selected period.</p>
+						<p>The full period includes {daysTotal} days.</p>
+						<p>You should have spent no more than {base} a day.</p>
+						<p>This is your base limit for a full period.</p>
+					</div>
+
+				)
+
+				/*`
+					Your income is ${ summNetIncome } for the selected period.
+					The full period includes ${ daysTotal } days.
+					You should have spent no more than ${ base } a day.
+					This is your base limit for a full period.
+				`*/
+
+			case 'corrected':
+				return (
+					<div>
+						<p>You spent {summs.expenses} for the past period from {period.begin} to {lastDateWithExpense}.</p>
+						<p>Your current balance is {currentBalance} for remaining period {daysRest} days.</p>
+						<p>Now you can not spend more than {corrected} a day.</p>
+						<p>This is your corrected limit which should be followed in the remainder of the period.</p>
+					</div>
+				)
+
+				/*`
+					You spent ${ summs.expenses } for the past period from ${ period.begin } to ${ lastDateWithExpense }.
+					Your current balance is ${ currentBalance } for remaining period ${ daysRest } days.
+					Now you can not spend more than ${ corrected } a day.
+					This is your corrected limit which should be followed in the remainder of the period.
+				`*/
+
+			case 'fact':
+				return (
+					<div>
+						<p>You spent {summs.expenses} for the past period from {period.begin} to {lastDateWithExpense}.</p>
+						<p>It is {fact} for a day in average.</p>
+						<p>If you will spend {fact} in day for remaining period {daysRest} days,
+							expected balance wil be {result} on end of period.</p>
+						<p>It is recommended to adhere to the corrected limit {corrected} for a day.</p>
+					</div>
+				)
+				
+				/*`
+					You spent ${ summs.expenses } for the past period from ${ period.begin } to ${ lastDateWithExpense }.
+					It is ${ fact } for a day in average.
+					If you will spend ${ fact } in day for remaining period ${ daysRest } days,
+					expected balance wil be ${ result } on end of period.
+					It is recommended to adhere to the corrected limit ${ corrected } for a day.
+				`*/
+
+			default:
+				return ''
+		}
+	}
+	
+}
